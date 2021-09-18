2115a132eae35deb6ddf5babb118af975e334f4c
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-18 13:21:53 -0700

    initial commit

--- part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -5,13 +5,7 @@ public class AdaLovelace {
     public static void main(String[] args) {
         // Write your program here
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("Give a number: ");
-        int year = Integer.valueOf(input.nextLine());
-        
-        if (year == 1984) {
-            System.out.println("Orwell");
-        }
-         
+        String nimi = "Ada Lovelace";
+        System.out.println(nimi);
     }
 }
