1111fb403466ee20220dcf9015243d27e53e220e
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-19 10:23:35 -0700

    add solutions to exercises

--- part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -3,7 +3,7 @@ public class HiAdaLovelace {
 
     public static void main(String[] args) {
         String name = "Ada Lovelace";
-
-
+        // Print name
+        System.out.println("Hi " + name + "!");
     }
 }
--- part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -6,7 +6,26 @@ public class DifferentTypesOfInput {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
-
+        // Write a string
+        System.out.println("Give a string:");
+        String str = scan.nextLine();
+        
+        // Write an integer
+        System.out.println("Give an integer:");
+        int num = Integer.valueOf(scan.nextLine());
+        
+        // Write a double
+        System.out.println("Give a double:");
+        double flt = Double.valueOf(scan.nextLine());
+        
+        // Write a boolean
+        System.out.println("Give a boolean:");
+        boolean bool = Boolean.valueOf(scan.nextLine());
+        
+        // print all types with a message
+        System.out.println("You gave the string " + str);
+        System.out.println("You gave the integer " + num);
+        System.out.println("You gave the double " + flt);
+        System.out.println("You gave the boolean " + bool);
     }
 }
