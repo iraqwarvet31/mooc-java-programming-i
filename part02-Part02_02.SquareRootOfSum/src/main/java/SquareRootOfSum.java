1826aba7271cbd71444034c634269b7f429581e5
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-20 12:53:17 -0700

    add solutions

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,14 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int first, second, sum;
+        
+        // Get numbers from user then print square root
+        first = Integer.valueOf(scanner.nextLine());
+        second = Integer.valueOf(scanner.nextLine());
+        sum = first + second;
+        
+        // Print square root of the sum
+        System.out.println((int) (Math.sqrt(sum)));
     }
 }
--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,15 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        // Get number from user
+        int num = Integer.valueOf(scanner.nextLine());
+        
+        // if num is negative print its absolute, otherwsise print num
+        if (num < 0) {
+            System.out.println(Math.abs(num));
+        } else {
+            System.out.println(num);
+        }
     }
 }
