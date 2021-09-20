63cdf968e5f6bc3d4705dc417df65d786ea64d0c
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-20 13:45:22 -0700

    add solutions

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -5,6 +5,18 @@ public class ComparingNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        // Get first and second nums from user
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        // Find smallest number and print
+        if (first > second) {
+            System.out.println(first + " is greater than " + second + ".");
+        } else if (first < second) {
+            System.out.println(first + " is smaller than " + second + ".");
+        } else {
+            System.out.println(first + " is equal to " + second + ".");
+        }
     }
 }
