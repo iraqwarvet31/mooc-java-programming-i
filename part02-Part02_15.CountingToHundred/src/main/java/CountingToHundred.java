37211d6dbda77e0a8c956b01c284fda94b192f60
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-21 12:28:57 -0700

    add solutions

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,13 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        // Get integer from user
+        int num = Integer.valueOf(scanner.nextLine());
+        
+        // Print to a hundred starting from given num
+        for (int i = num; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }
