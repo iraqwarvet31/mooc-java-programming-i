cbea89a52f132de85efa1c6d48b8aa90e2304c81
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-23 11:44:12 -0700

    add another set of solutions to part 3

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,24 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String oldest = "";
+        Integer greatest = 0;
+        
+        while (true) {
+            String csv = scanner.nextLine();
+            if (csv.isEmpty()) {
+                break;
+            }
+            
+            String[] parts = csv.split(",");
+            String name = parts[0];
+            int age = Integer.valueOf(parts[parts.length - 1]);
+            
+            if (age > greatest) {
+                greatest = age;
+                oldest = name;
+            }
+        }
+        System.out.println("Name of the oldest: " + oldest);
     }
 }
--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,31 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        ArrayList<String> users = new ArrayList<>();
+        int sum = 0;
+        String longest = "";
+        
+        while (true) {
+            String user = scanner.nextLine();
+            
+            if (user.isEmpty()) {
+                break;
+            }
+            users.add(user);
+        }
+        
+        for (String item : users) {
+            String[] pieces = item.split(",");
+            String name = pieces[0];
+            int year = Integer.valueOf(pieces[1]);
+            
+            if (name.length() > longest.length()) {
+                longest = name;
+            }
+            sum += year;
+        }
+        
+        System.out.println("Longest name: " + longest);
+        System.out.println("Average of the birth years: " + (double) sum / users.size());
     }
 }
