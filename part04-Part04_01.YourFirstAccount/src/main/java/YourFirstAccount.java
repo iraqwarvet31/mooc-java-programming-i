1cf361c0570571426654be8482481f94bdd599db
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-24 06:43:39 -0700

    add part 4 questions

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account larrysAccount = new Account("Larrys Bank of America account", 100);
+        larrysAccount.deposit(20.0);
+        System.out.println(larrysAccount);
+    }
+}
