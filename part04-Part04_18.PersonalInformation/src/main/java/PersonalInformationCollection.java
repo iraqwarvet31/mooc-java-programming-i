dae4c6cbc6ca5eb27670dc5287a2ef9e0c5abf38
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-25 16:15:25 -0700

    add solutions

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,25 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            // Grab first name, last name, and identification from person
+            System.out.println("First name: ");
+            String first = scanner.nextLine();
+            if (first.isEmpty()) {
+                break;
+            }
+            System.out.println("Last name: ");
+            String last = scanner.nextLine();
+            System.out.println("Identification number: ");
+            String id = scanner.nextLine();
+
+            // create instance of this person and add to infoCollection
+            infoCollection.add(new PersonalInformation(first, last, id));
+        }
+
+        for (PersonalInformation info : infoCollection) {
+            System.out.println(info.getFirstName() + " " + info.getLastName());
+        }
+
     }
 }
--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,29 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String title = scanner.nextLine();
+            if (title.isEmpty()) {
+                break;
+            }
+            
+            System.out.print("Duration: ");
+            int minutes = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram(title, minutes));
+        }
+        
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        
+        for (TelevisionProgram program : programs) {
+            int duration = program.getDuration();
+            String name = program.getName();
+            
+            if (duration <= maxDuration) {
+                System.out.println(name + ", " + duration + " minutes");
+            }
+        }
     }
 }
