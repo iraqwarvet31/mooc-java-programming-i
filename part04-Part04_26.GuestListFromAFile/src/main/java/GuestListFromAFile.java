de34cfe367b4babbb66feb324a06613ceab3af3d
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-26 20:31:50 -0700

    add solution

--- part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -7,15 +7,29 @@ public class GuestListFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        // Ask user for file name
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
-
+        
+        // Array of strings from file
         ArrayList<String> list = new ArrayList<>();
-        // implement reading the file here.
         System.out.println("");
-
+        
+        // Read the file and place each row in list array
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                list.add(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        // Ask user for a name
         System.out.println("Enter names, an empty line quits.");
+        
+        // Check for name in list and handle accordingly
         while (true) {
             String name = scanner.nextLine();
             if (name.isEmpty()) {
