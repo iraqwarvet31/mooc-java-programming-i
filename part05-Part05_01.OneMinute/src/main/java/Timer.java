e4d817a2bc0af53c8e57a6308e0a2a4e130faa31
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-28 13:45:34 -0700

    add partial solution

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author justadev
+ */
+public class Timer {
+    private int hundredsOfSeconds;
+    private int seconds;
+    
+    public Timer () {
+        this.hundredsOfSeconds = 0;
+        this.seconds = 0;
+    }
+    
+    public void advance() {
+        
+    }
+}
