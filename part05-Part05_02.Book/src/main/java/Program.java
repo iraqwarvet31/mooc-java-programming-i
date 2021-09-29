c8bb0b790381e1e87acffe7748fc6b62a226b325
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-29 11:23:05 -0700

    add solutions

--- part05-Part05_02.Book/src/main/java/Program.java
+++ part05-Part05_02.Book/src/main/java/Program.java
@@ -6,9 +6,11 @@ public class Program {
         // free to experiment with your Book class.
         // An example that you can use for testing:
 
-
-//        Book b = new Book("J. K. Rowling", "Harry Potter and the Sorcerer's Stone", 223);
-//        System.out.println(b);
-
+        Book b = new Book("KonMari", "Marie Kondo", 222);
+        System.out.println(b);
+        
+        System.out.println("Name: " + b.getName());
+        System.out.println("Author: " + b.getAuthor());
+        System.out.println("Pages: " + b.getPages());
     }
 }
--- part05-Part05_03.Cube/src/main/java/Program.java
+++ part05-Part05_03.Cube/src/main/java/Program.java
@@ -7,5 +7,13 @@ public class Program {
         Scanner scanner = new Scanner(System.in);
 
         // Experiment with your program here
+        Cube c = new Cube(3);
+        System.out.println(c);
+        
+        Cube cc = new Cube(4);
+        System.out.println(cc);
+        
+        Cube ccc = new Cube(2);
+        System.out.println(ccc);
     }
 }
