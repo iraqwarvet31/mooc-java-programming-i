a72fdd2b69bc9d983b908d97e7b9356c0d68fc85
Larry Olguin <iraqwarvet31@gmail.com>
2021-09-30 08:58:17 -0700

    add solution

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,25 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter d = new Counter(10);
+        Counter i = new Counter();
+        
+        d.decrease();
+        d.decrease();
+        d.decrease();
+        System.out.println("Counter 'd' value is now: " + d.value());
+        
+        i.increase();
+        i.increase();
+        i.increase();
+        i.increase();
+        i.increase();
+        System.out.println("Counter 'i' value is now: " + i.value());
+        
+        d.decrease(5);
+        System.out.println("Decrease by 5: " + d.value());
+        
+        i.increase(5);
+        System.out.println("Increase by 5: " + i.value());
     }
 }
