053a53bd8078f6b47ecdf76e858fb8ca179b0d16
Larry Olguin <iraqwarvet31@gmail.com>
2021-10-02 11:07:13 -0700

    add solutions

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,22 @@
 
 public class HealthStation {
-
-
+    private int numberOfWeighings;
+    
+    public HealthStation () {
+        this.numberOfWeighings = 0;
+    }
+    
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        this.numberOfWeighings++;
+        return person.getWeight();
     }
-
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings() {
+        return this.numberOfWeighings;
+    }
 }
--- part05-Part05_09.HealthStation/src/main/java/Main.java
+++ part05-Part05_09.HealthStation/src/main/java/Main.java
@@ -3,5 +3,22 @@ public class Main {
 
     public static void main(String[] args) {
         // write experimental code here to check how your program functions
+        HealthStation kaiser = new HealthStation();
+        
+        Person larry = new Person("Larry", 40, 59, 195);
+        
+        System.out.println(larry.getName() + " weight: " + kaiser.weigh(larry) + " lbs.");
+        kaiser.feed(larry);
+        kaiser.feed(larry);
+        kaiser.feed(larry);
+        kaiser.feed(larry);
+        kaiser.feed(larry);
+        System.out.println("Larry is fed at kaiser and now weighs: " + larry.getWeight() + " lbs.");
+        kaiser.weigh(larry);
+        kaiser.weigh(larry);
+        kaiser.weigh(larry);
+        kaiser.weigh(larry);
+        kaiser.weigh(larry);
+        System.out.println("Larry performed: " + kaiser.weighings() + " weighings!");
     }
 }
